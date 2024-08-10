cb472518bbf2e09386ee3831af74d84ff7d90287
bubbliorno <dasparkleguy@gmail.com>
2024-08-09 22:31:49 -0400

    decreased image dimensions in gallery page

--- js/gallery_masonry.js
+++ js/gallery_masonry.js
@@ -3,7 +3,7 @@ window.onload = () => {
     // Sets the grid var to .container
     const grid = document.querySelector('.container');
 
-    // 
+    // Creates new Masonry grid and defines it
     const masonry = new Masonry(grid, {
         itemSelector: 'img',
         columnWidth: '.container img',
